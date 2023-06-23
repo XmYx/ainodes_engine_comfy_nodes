df4425ef83f9258d147d324616471c21c68a9c56
unknown <miklos.mnagy@gmail.com>
2023-06-23 23:04:02 +0200

    better adapter - full native input/output logic

--- adapter_nodes/adapter_utils.py
+++ adapter_nodes/adapter_utils.py
@@ -2,7 +2,7 @@ import numpy as np
 import torch
 from PIL import Image
 
-from ai_nodes.ainodes_engine_base_nodes.ainodes_backend import pixmap_to_pil_image, pil_image_to_pixmap
+from ai_nodes.ainodes_engine_base_nodes.ainodes_backend import pixmap_to_tensor, tensor_image_to_pixmap
 
 loadBackup = torch.load
 
@@ -13,7 +13,7 @@ from ainodes_frontend.base import register_node, AiNode, register_node_now, get_
 from ainodes_frontend.node_engine.node_content_widget import QDMNodeContentWidget
 
 
-defaults = {"FLOAT":{"min":0.0,
+default_numeric = {"FLOAT":{"min":0.0,
                      "max":100.0,
                      "default":1.0,
                      "step":0.01},
@@ -21,12 +21,14 @@ defaults = {"FLOAT":{"min":0.0,
                      "max":100,
                      "default":1,
                      "step":1},
-            "NUMBER":  {"min":0,
+            "NUMBER":{"min":0,
                      "max":1,
                      "default":1,
                      "step":1},
             }
 
+
+data_subtypes = ["STRING", "PROMPT", "NUMBER", "FLOAT"]
 
 def tensor2pil(image):
     return Image.fromarray(np.clip(255. * image.cpu().numpy().squeeze(), 0, 255).astype(np.uint8))
@@ -36,94 +38,92 @@ def tensor2pil(image):
 def pil2tensor(image):
     return torch.from_numpy(np.array(image).astype(np.float32) / 255.0).unsqueeze(0)
 
-def create_node(node_class, node_name, ordered_inputs, inputs, ordered_outputs, outputs, fn):
-
-    input_names = ordered_inputs.pop(len(ordered_inputs) - 1)
-    output_names = ordered_outputs.pop(len(ordered_outputs) - 1)
 
 
-    #print("INPUT NAMES BECAME", input_names)
+
+def create_node(node_class, node_name, ui_inputs, inputs, input_names, outputs, output_names, category_input):
+
+
 
     class_name = node_name.replace(" ", "")
-    #class_code = "OP_NODE_" + class_name.upper()
     class_code = get_next_opcode()
 
     # Create new Widget class
     class Widget(QDMNodeContentWidget):
+        def get_widget(self, widget_type):
+            widget_types = {"INT":self.create_spin_box,
+                            "FLOAT":self.create_double_spin_box,
+                            "STRING":self.create_line_edit,
+                            "MULTISTRING":self.create_text_edit
+                            }
+            if widget_type in widget_types:
+                return widget_types[widget_type], True
+            else:
+                return self.create_line_edit, False
+
+        def create_widget(self, widget_name, widget_type, widget_params):
+
+            widget, known = self.get_widget(widget_type)
+
+            if known:
+                if widget_type in default_numeric.keys():
+
+                    min_val = default_numeric[widget_type]['min']
+                    max_val = default_numeric[widget_type]['max']
+                    def_val = default_numeric[widget_type]['default']
+                    step_val = default_numeric[widget_type]['step']
+                    if 'min' in widget_params:
+                        min_val = widget_params['min']
+                    if 'max' in widget_params:
+                        max_val = widget_params['max']
+                    if 'default' in widget_params:
+                        def_val = widget_params['default']
+                    if 'step' in widget_params:
+                        step_val = widget_params['step']
+                    if min_val < -2147483648:
+                        min_val = -2147483647
+                    if max_val > 2147483647:
+                        max_val = 2147483647
+                    setattr(self, widget_name, widget(label_text=widget_name, min_val=min_val, max_val=max_val, step=step_val, default_val=def_val))
+                else:
+                    default = 'default_placeholder'
+                    multiline = False
+                    if 'multiline' in widget_params:
+                        multiline = widget_params['multiline']
+                    if 'default' in widget_params:
+                        default = widget_params['default']
+                    if widget_type == 'PROMPT':
+                        multiline = True
+                        default = "Prompt"
+                    if multiline:
+                        setattr(self, widget_name, self.create_text_edit(widget_name, default=default))
+                        #height += 100
+
+                    else:
+                        setattr(self, widget_name, self.create_line_edit(widget_name, default=default, placeholder=default))
+                        #height += 50
+            else:
+                setattr(self, widget_name, widget)
+
         def initUI(self):
-
-            self.input_adapted = []
-            height = 0
-            for input_item in ordered_inputs:
-
-                #print("INPUT ITEM", input_item)
-
-                tp = input_item[1][0]
+            for item in ui_inputs:
+                #print(item)
+                tp = item[1][0]
                 if type(tp) == str:
-                    source = input_item[1]
-
-                    if tp in ['FLOAT', 'INT', 'NUMBER']:
-                        min_val = defaults[tp]['min']
-                        max_val = defaults[tp]['max']
-                        def_val = defaults[tp]['default']
-                        step_val = defaults[tp]['step']
-                        #print("SOURCE", source)
-                        if len(list(source)) > 1:
-                            if 'min' in source[1]:
-                                min_val = source[1]['min']
-                            if 'max' in source[1]:
-                                max_val = source[1]['max']
-                            if 'default' in source[1]:
-                                def_val = source[1]['default']
-                            if 'step' in source[1]:
-                                step_val = source[1]['step']
-                        if min_val < -2147483648:
-                            min_val = -2147483647
-                        if max_val > 2147483647:
-                            max_val = 2147483647
-                        if tp == 'FLOAT':
-                            setattr(self, input_item[0], self.create_double_spin_box(label_text=input_item[0], min_val=min_val, max_val=max_val, step=step_val, default_val=def_val))
-                        elif tp in ['INT', 'NUMBER']:
-                            setattr(self, input_item[0], self.create_spin_box(label_text=input_item[0], min_val=int(min_val), max_val=int(max_val), step_value=int(step_val), default_val=int(def_val)))
-                        height += 50
-                    elif tp in ['STRING', 'CROP_DATA', 'IMAGE_BOUNDS', 'PROMPT']:
-
-
-                        default = 'default_placeholder'
-                        multiline = False
-                        if len(list(source)) > 1:
-                            if 'multiline' in source[1]:
-                                multiline = source[1]['multiline']
-                            if 'default' in source[1]:
-                                default = source[1]['default']
-                        if tp == 'PROMPT':
-                            multiline = True
-                            default = "Prompt"
-                        if multiline:
-                            setattr(self, input_item[0], self.create_text_edit(input_item[0], default=default))
-                            tp = 'MULTI_STRING'
-                            height += 100
-
-                        else:
-                            setattr(self, input_item[0], self.create_line_edit(input_item[0], default=default, placeholder=default))
-                            height += 50
-
-                    self.input_adapted.append({"type":tp,
-                                               "name":input_item[0]})
-
-
-
+                    # source = item[1]
+                    name = item[0]
+                    widget_type = item[1][0]
+                    widget_params = item[1][1]
+                    self.create_widget(name, widget_type, widget_params)
                 elif type(tp) == list:
+                    combobox_name = item[0]
+                    combobox_items = item[1][0]
+                    setattr(self, combobox_name,
+                            self.create_combo_box(combobox_items, combobox_name, accessible_name=combobox_name))
+                else:
+                    print("OTHER ITEM", type(tp), item)
 
-                    setattr(self, input_item[0],
-                            self.create_combo_box(input_item[1][0], input_item[0], accessible_name=input_item[0]))
-                    self.input_adapted.append({"type":"COMBOBOX",
-                                               "name":input_item[0]})
-                    height += 50
-            print("ADAPTED INPUT LIST", self.input_adapted)
             self.create_main_layout(grid=1)
-            #self.setMinimumHeight(height)
-            #self.setMaximumHeight(height)
 
 
     # Create new Node class
@@ -138,11 +138,11 @@ def create_node(node_class, node_name, ordered_inputs, inputs, ordered_outputs, 
         op_code = class_code
         op_title = node_name
         content_label_objname = class_name.lower().replace(" ", "_")
-        category = f"ComfyUI_Extras/{node_class.CATEGORY}"#"WAS NODES"
+        category = f"{category_input}/{node_class.CATEGORY}"#"WAS NODES"
         NodeContent_class = Widget
         dim = (340, 180)
         output_data_ports = outputs
-        exec_port = 0
+        exec_port = len(outputs)
 
         custom_input_socket_name = input_names
         custom_output_socket_name = output_names
@@ -150,8 +150,10 @@ def create_node(node_class, node_name, ordered_inputs, inputs, ordered_outputs, 
         def __init__(self, scene):
             super().__init__(scene, inputs=inputs, outputs=outputs)
 
-            self.fn = fn
-            self.ordered_outputs = ordered_outputs
+            self.fn = getattr(node_class, node_class.FUNCTION)
+
+            self.output_names = output_names
+            self.output_types = outputs
 
             self.exec_port = len(self.outputs) - 1
 
@@ -165,88 +167,73 @@ def create_node(node_class, node_name, ordered_inputs, inputs, ordered_outputs, 
 
             self.update_all_sockets()
 
+            self.adapted_inputs = (input_names, ui_inputs)
+            self.adapted_outputs = output_names
+
         def evalImplementation_thread(self):
 
             data_inputs = {}
             x = 0
-            for adapted_input in self.content.input_adapted:
-                data = f"Not Found {adapted_input['name']}"
-                if adapted_input['type'] in ['LATENT', 'IMAGE', 'CONDITIONING', 'EXTRA_PNGINFO', "VAE", "CLIP", "MODEL", "GUIDED_DIFFUSION_MODEL", "GUIDED_CLIP", "EXTRA_SETTINGS", "DISCO_DIFFUSION_EXTRA_SETTINGS"]:
-                    input_data = self.getInputData(x)
-                    data = input_data
-                    x += 1
-                if adapted_input['type'] == 'IMAGE':
-                    if input_data is not None:
-                        input_data = pixmap_to_pil_image(input_data[0])
-                        data = pil2tensor(input_data)
-                elif adapted_input['type'] in ["EXTRA_SETTINGS", "DISCO_DIFFUSION_EXTRA_SETTINGS"] and input_data == None:
-                    data = {}
-                elif adapted_input['type'] == ["LATENT"]:
-                    data = {"samples":input_data[0]}
-                elif adapted_input['type'] == ["CONDITIONING"]:
-                    data = input_data[0]
-                elif adapted_input['type'] in ['FLOAT', 'INT']:
-                    data = getattr(self.content, adapted_input['name']).value()
-                elif adapted_input['type'] == 'STRING':
-                    data = getattr(self.content, adapted_input['name']).text()
-                elif adapted_input['type'] in ['MULTI_STRING', 'PROMPT']:
-                    data = getattr(self.content, adapted_input['name']).toPlainText()
-                elif adapted_input['type'] in ['CROP_DATA', 'IMAGE_BOUNDS']:
-                    data = getattr(self.content, adapted_input['name']).text()
-                    data = data.split(",")
-                    data = tuple(int(item) for item in data)
-                elif adapted_input['type'] == 'COMBOBOX':
-                    data = getattr(self.content, adapted_input['name']).currentText()
-                if adapted_input['name'] in ["latent_image"]:
-                    data_inputs[adapted_input['name']] = {"samples":data[0]}
-                elif adapted_input['name'] in ["samples"]:
-                    data_inputs[adapted_input['name']] = {"samples":data}
-                elif adapted_input['name'] in ['positive', 'negative']:
-                    data_inputs[adapted_input['name']] = data[0]
-                else:
-                    data_inputs[adapted_input['name']] = data
 
-            # for i in range(len(self.inputs)):
-            #     data = self.getInputData(x)
-            #     data_inputs.append(data)
-            #     x += 1
-            #     print(x)
+            for input in self.adapted_inputs[0]:
+                data = None
+                if input != "EXEC":
 
-            #print(data_inputs)
+                    data = self.getInputData(x)
 
-            print("COLLECTED COMFY INPUTS", data_inputs)
+
+                if data is not None:
+                    data_inputs[input.lower()] = data
+                x += 1
+
+
+            for ui_input in self.adapted_inputs[1]:
+
+                input_type = ui_input[1][0]
+                input_name = ui_input[0]
+                widget = getattr(self.content, input_name)
+
+
+                if isinstance(input_type, str):
+                    if input_type in default_numeric.keys():
+                        data = widget.value()
+                        data_inputs[ui_input[0].lower()] = data
+                    else:
+                        multiline = False
+                        if "multiline" in ui_input[1][1]:
+                            multiline = ui_input[1][1]
+                        if multiline:
+                            data = widget.toPlainText()
+                        else:
+                            data = widget.text()
+                        if data != "":
+                            data_inputs[ui_input[0].lower()] = data
+                elif isinstance(input_type, list):
+                    data = widget.currentText()
+                    if data != "":
+                        data_inputs[ui_input[0].lower()] = data
 
             result = self.fn(self, **data_inputs)
+
             x = 0
-            pixmaps = []
-            for output in self.ordered_outputs:
-                if output['type'] == 'IMAGE':
-                    image = result[x]
-                    if isinstance(image, list):
-                        for img in image:
-                            img = img.detach().cpu()
-                            pil = tensor2pil(img)
-                            pixmaps.append(pil_image_to_pixmap(pil))
-                    else:
-                        image = image.detach().cpu()
-                        pil = tensor2pil(image)
-                        pixmaps = [pil_image_to_pixmap(pil)]
-                    self.setOutput(x, pixmaps)
-                elif output['type'] == 'LATENT':
-                    if isinstance(result[x], dict):
-                        samples = result[x]["samples"].detach().cpu()
-                    elif isinstance(result[x], list):
-                        samples = result[x][0].detach().cpu()
-                    else:
-                        samples = result[x].detach().cpu()
-                    self.setOutput(x, samples)
-                else:
+
+            for i in list(self.adapted_outputs):
+
+                if i != "EXEC":
+                    print(i, x, result[x])
                     self.setOutput(x, result[x])
-            print("RAN COMFY NODE", self.op_title, "in aiNodes Engine")
+                x += 1
+
+            return True
+
+        def onWorkerFinished(self, result):
+            self.busy = False
+            self.markDirty(False)
+            if result:
+                self.executeChild(self.exec_port)
 
 
 
-    # Register the node
     register_node_now(class_code, Node)
 
 
--- adapter_nodes/was_adapter_node.py
+++ adapter_nodes/was_adapter_node.py
@@ -4,7 +4,9 @@ import importlib
 import time
 import traceback
 
-from ai_nodes.ainodes_engine_comfy_nodes.adapter_nodes.adapter_utils import parse_comfynode
+from ai_nodes.ainodes_engine_comfy_nodes.adapter_nodes.adapter_utils import create_node
+
+# from ai_nodes.ainodes_engine_comfy_nodes.adapter_nodes.adapter_utils import parse_comfynode
 
 WAS = "GREAT"
 
@@ -30,7 +32,6 @@ def load_comfy_node(module_path):
 
             mappings = getattr(module, "NODE_CLASS_MAPPINGS")
 
-            print("FOUND MAPPING", mappings)
 
             return mappings
 
@@ -64,6 +65,7 @@ def load_comfy_nodes():
             time_before = time.perf_counter()
             mapping = load_comfy_node(module_path)
             if mappings is not None:
+                #print(mapping)
                 mappings.append(mapping)
             node_import_times.append((time.perf_counter() - time_before, module_path, True))
 
@@ -75,31 +77,163 @@ def load_comfy_nodes():
             else:
                 import_message = " (IMPORT FAILED)"
             print("{:6.1f} seconds{}:".format(n[0], import_message), n[1])
-        print()
-
+    #import nodes
+    #mappings.append(nodes.NODE_CLASS_MAPPINGS)
     return mappings
 
 
-
-
+possible_ports = []
+possible_ui_elements = []
+possible_output_ports = []
 
 directory = "src/ComfyUI/custom_nodes"
+def get_node_parameters(node_class):
+
+    global possible_ports
+    global possible_ui_elements
+
+    ordered_inputs = []
+    ordered_outputs = []
+
+    params = {"class":node_class}
+
+
+    for key, value in node_class.INPUT_TYPES().items():
+        for value_name, value_params in value.items():
+
+            is_port = isinstance(value_params, str)
+
+            if not is_port and len(value_params) == 1:
+
+                if isinstance(value_params[0], str):
+
+                    is_port = True
+
+            if is_port:
+
+                if isinstance(value_params, str):
+                    n = value_params
+                else:
+                    n = value_params[0]
+
+                if n not in possible_ports:
+                    possible_ports.append(n)
+            else:
+                if value_name not in possible_ui_elements:
+                    possible_ui_elements.append((value_name, value_params))
+
+
+            #print(value_name, value_params, type(value_params), "len", len(value_params), "UI" if not is_port else "PORT")
+            ordered_inputs.append((value_name, value_params, "UI" if not is_port else "PORT"))
+    params["inputs"] = ordered_inputs
+    if isinstance(node_class.RETURN_TYPES, tuple):
+        for item in node_class.RETURN_TYPES:
+            if item.upper() not in possible_output_ports:
+                possible_output_ports.append(item.upper())
+            ordered_outputs.append(item.upper())
+    elif isinstance(node_class.RETURN_TYPES, str):
+        if node_class.RETURN_TYPES.upper() not in possible_output_ports:
+            possible_output_ports.append(node_class.RETURN_TYPES.upper())
+        ordered_outputs.append(node_class.RETURN_TYPES.upper())
+
+
+    #print("PARAMS", params)
+
+    return ordered_inputs, ordered_outputs
+
+# print(possible_ui_elements)
+
+known_ports = {"LATENT":2,
+               "CONDITIONING":3,
+               "IMAGE":5,
+               "MASK":5,
+               "VAE":4,
+               "CLIP":4,
+               "MODEL":4,
+               "CONTROL_NET":4,
+               }
+
+def parse_comfynode(node_name, node_class, category):
+    node_content_class = node_name.lower().replace(" ", "_")
+    # print(node_class.INPUT_TYPES())
+    ordered_inputs, ordered_outputs = get_node_parameters(node_class)
+    inputs = []
+    input_names = []
+    ui_inputs = []
+    for i in ordered_inputs:
+        if i[2] == "PORT":
+            if i[1][0] in known_ports:
+                inputs.append(known_ports[i[1][0]])
+            else:
+                inputs.append(7)
+            input_names.append(i[0].upper())
+        else:
+            ui_inputs.append(i)
+
+            #print("SEARCHING HERE", i)
+            if len(list(i[1])) > 1:
+
+                if "defaultBehavior" in i[1][1]:
+                    inputs.append(7)
+                    input_names.append(i[0].upper())
+
+    inputs.append(1)
+    input_names.append('EXEC')
+
+    outputs = []
+    output_names = []
+
+    for i in ordered_outputs:
+        if i in known_ports:
+            outputs.append(known_ports[i])
+        else:
+            outputs.append(7)
+        output_names.append(i)
+
+    outputs.append(1)
+    output_names.append('EXEC')
+    print("creating_node", node_class)
+    node = create_node(node_class=node_class,
+                       node_name=node_name,
+                       ui_inputs=ui_inputs,
+                       inputs=inputs,
+                       input_names=input_names,
+                       outputs=outputs,
+                       output_names=output_names,
+                        category_input=category)
 
 
 
 
 
-import nodes
+
+#import nodes
 #from custom_nodes.ComfyUI_Disco_Diffusion.nodes import NODE_CLASS_MAPPINGS
 
 #print("Comfy Node Class Mappings", nodes.NODE_CLASS_MAPPINGS)
 
-node_class_mappings = load_comfy_nodes()
 
-node_class_mappings.append(nodes.NODE_CLASS_MAPPINGS)
+
+#node_class_mappings.append(nodes.NODE_CLASS_MAPPINGS)
+
+# for mapping in node_class_mappings:
+#     if mapping is not None:
+#         #print(mapping)
+import nodes
+for node_name, node_class in nodes.NODE_CLASS_MAPPINGS.items():
+    parse_comfynode(node_name, node_class, "ComfyUI Base")
+
+node_class_mappings = load_comfy_nodes()
 
 for mapping in node_class_mappings:
     if mapping is not None:
-        print(mapping)
         for node_name, node_class in mapping.items():
-            parse_comfynode(node_name, node_class)
+            parse_comfynode(node_name, node_class, "ComfyUI Extras")
+
+
+
+
+
+# print("PORTS", possible_ports)
+# print("UI", possible_ui_elements)
+# print("OUT", possible_output_ports)
